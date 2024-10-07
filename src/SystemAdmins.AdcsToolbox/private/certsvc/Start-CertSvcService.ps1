e9556d9a7cb320dcd04c34d43ba96f8a8b22fb88
Alex Hansen <ath@systemadmins.com>
2024-10-07 14:24:03 +0200

    chore: Refactor CertSvc service start and stop scripts into separate files

---
+++ src/SystemAdmins.AdcsToolbox/private/certsvc/Start-CertSvcService.ps1
@@ -0,0 +1,70 @@
+function Start-CertSvcService
+{
+    <#
+    .SYNOPSIS
+        Start the CertSvc service.
+    .DESCRIPTION
+        Returns nothing.
+    .EXAMPLE
+        Start-CertSvcService;
+    #>
+    [cmdletbinding()]
+    [OutputType([void])]
+    param
+    (
+    )
+
+    BEGIN
+    {
+        # Write to log.
+        $progressId = Write-CustomProgress -Activity $MyInvocation.MyCommand.Name -CurrentOperation 'Starting CertSvc service' -Type 'Start';
+
+        # Service name.
+        [string]$serviceName = 'CertSvc';
+    }
+    PROCESS
+    {
+        # Try to get the service.
+        try
+        {
+            # Get the service.
+            [System.ServiceProcess.ServiceController]$service = Get-Service -Name $serviceName -ErrorAction Stop;
+
+            # If service is running.
+            if ($service.Status -eq 'Running')
+            {
+                # Write to log.
+                Write-CustomLog -Message ("Service '{0}' is already running" -f $serviceName) -Level Verbose;
+            }
+            # Else service is not running.
+            else
+            {
+                # Try to stop the service.
+                try
+                {
+                    # Stop the service.
+                    $null = Start-Service -Name $serviceName -ErrorAction Stop;
+
+                    # Write to log.
+                    Write-CustomLog -Message ("Service '{0}' started" -f $serviceName) -Level Verbose;
+                }
+                # Something went wrong.
+                catch
+                {
+                    # Throw execption.
+                    throw ("Failed to start service '{0}'. {1}" -f $serviceName, $_.Exception.Message);
+                }
+            }
+        }
+        # Something went wrong.
+        catch
+        {
+            throw ("Something went wrong while starting the service '{0}'. {1}" -f $serviceName, $_.Exception.Message);
+        }
+        END
+        {
+            # Write to log.
+            Write-CustomProgress -Id $progressId -Activity $MyInvocation.MyCommand.Name -CurrentOperation 'Starting CertSvc service' -Type 'End';
+        }
+    }
+}
---
+++ src/SystemAdmins.AdcsToolbox/private/certsvc/Stop-CertSvcService.ps1
@@ -0,0 +1,71 @@
+function Stop-CertSvcService
+{
+    <#
+    .SYNOPSIS
+        Stop the CertSvc service.
+    .DESCRIPTION
+        Returns nothing.
+    .EXAMPLE
+        Stop-CertSvcService;
+    #>
+    [cmdletbinding()]
+    [OutputType([void])]
+    param
+    (
+    )
+
+    BEGIN
+    {
+        # Write to log.
+        $progressId = Write-CustomProgress -Activity $MyInvocation.MyCommand.Name -CurrentOperation 'Stopping CertSvc service' -Type 'Start';
+
+        # Service name.
+        [string]$serviceName = 'CertSvc';
+    }
+    PROCESS
+    {
+        # Try to get the service.
+        try
+        {
+            # Get the service.
+            [System.ServiceProcess.ServiceController]$service = Get-Service -Name $serviceName -ErrorAction Stop;
+
+            # If service is running.
+            if ($service.Status -eq 'Running')
+            {
+                # Try to stop the service.
+                try
+                {
+                    # Stop the service.
+                    $null = Stop-Service -Name $serviceName -ErrorAction Stop;
+
+                    # Write to log.
+                    Write-CustomLog -Message ("Service '{0}' stopped" -f $serviceName) -Level Verbose;
+                }
+                # Something went wrong.
+                catch
+                {
+                    # Throw execption.
+                    throw ("Failed to stop service '{0}'. {1}" -f $serviceName, $_.Exception.Message);
+                }
+            }
+            # Else service is not running.
+            else
+            {
+                # Write to log.
+                Write-CustomLog -Message ("Service '{0}' is already stopped" -f $serviceName) -Level Verbose;
+            }
+        }
+        # Something went wrong.
+        catch
+        {
+            # Throw execption.
+            throw ("Something went wrong while trying to stop the service dont exist. {1}" -f $serviceName, $_.Exception.Message);
+        }
+    }
+    END
+    {
+        # Write to log.
+        Write-CustomProgress -Id $progressId -Activity $MyInvocation.MyCommand.Name -CurrentOperation 'Stopping CertSvc service' -Type 'End';
+    }
+}
